aaa5183b77c529dcb5b071f9bd7d0f52ca94d06f
PK <pruthvi.kumarbk@anz.com>
2019-07-11 05:49:07 +1000

    Some simple documentaiton

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ __author__='Pruthvi Kumar'
 # Extract, Scrape and Compare - Main entry-point.
 
 # Step1 Load Page.
-page = LoadPage('https://www.apricity.co.in', 'about')
-page_source = page.extractor()
-
-# Step2 Scrape page source into si
-scraper = ScrapePage(page_source, 'apricity')
-scraper.extractor()
+# page = LoadPage('https://www.apricity.co.in', 'about')
+# page_source = page.extractor()
+#
+# # Step2 Scrape page source into si
+# scraper = ScrapePage(page_source, 'apricity')
+# scraper.extractor()
 
 # Step3 Compare and report differences between two pages.
 comparator = ComparePages('./sink/apricity.txt', './sink/ap2.txt', 'apricity_v_1')
 comparator.compare()
 
 # Step4 Extract respective DOM contents leveraging difference report.
-extractor = ExtractContents('apricity_v_1', 'id')
+extractor = ExtractContents('apricity_v_1', 'class') # second parameter can be id/class
 extractor.extract_interested_dom_contents()
